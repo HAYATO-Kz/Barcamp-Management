b98e945ceb77e2946cbae383fa260787a7ce03e1
JPSKE15 <hokiz_1999@hotmail.com>
2018-10-15 18:46:15 +0700

    Login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import registerServiceWorker from './registerServiceWorker';
+import {Route,BrowserRouter,Switch} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+    <div>
+<Switch>
+<Route exact path='/' component={App} />
+<Route exact path='/login' component={Login} />
+</Switch>
+    </div>
+    </BrowserRouter>
+  ,document.getElementById('root')
+);
+registerServiceWorker();
