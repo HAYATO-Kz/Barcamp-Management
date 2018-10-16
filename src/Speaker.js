2365dd2cbfdd929d592cf630e22737c1e269c1b3
JPSKE15 <hokiz_1999@hotmail.com>
2018-10-16 19:15:02 +0700

    delete import that didn't use

--- src/Speaker.js
+++ src/Speaker.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import firebase from 'firebase'
 import "./Speaker.css"
-import {Button,Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown,
+import {Button,Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, UncontrolledDropdown,
         DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 class Speaker extends React.Component {
